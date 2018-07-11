035fc43100dbdc26906329b19733389c7d69f946
ubuntu <ubuntu@org1-manager.org1.net>
2018-07-11 14:18:29 -0400

    updated build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,26 +1,28 @@
-BASE_DIR=$PWD
+#!/bin/bash
 
-if [[ -d $GOPATH ]];then
+sudo apt-get install -y build-essential libltdl3-dev
+if [ ! -d ~/git/src/github.com/hyperledger ]; then
+	mkdir -p ~/git/src/github.com/hyperledger
+	cd ~/git/src/github.com/hyperledger/
+	git clone -b v1.1.0 https://gerrit.hyperledger.org/r/fabric
+else
+	echo 'fabric already in place'
+fi
 
-  ARCH=`uname -s | grep Darwin`
-  if [ "$ARCH" == "Darwin" ]; then
-    if [ ! `command -v go` ]; then
-      brew install go
-    fi
-  else
-    if [ ! `command -v go` ]; then        
-      apt install golang-go -y
-    fi  
-    apt install libtool libltdl-dev -y
-  fi  
-
-  apt install libtool libltdl-dev
-  mkdir -p /opt/gopath/src
-  export GOPATH=/opt/gopath
-  cd $GOPATH/src
-  mkdir -p github.com/hyperledger
-  cd github.com/hyperledger
-  git clone https://github.com/hyperledger/fabric.git
+if [[ -z "${GOPATH}" ]]; then
+	cd /tmp
+	wget --no-check-certificate https://storage.googleapis.com/golang/go1.9.3.linux-s390x.tar.gz
+	tar -xvf go1.9.3.linux-s390x.tar.gz
+	sudo mv -iv go /opt
+	cd ~
+	cp -ipv .bashrc .bashrc_orig
+	echo '' >> .bashrc
+	echo "export GOPATH=/home/$USER/git" >> .bashrc
+	echo export GOROOT=/opt/go >> .bashrc
+	echo "export PATH=/opt/go/bin:/home/$USER/bin:\$PATH" >> .bashrc
+	echo '' >> .bashrc
+else
+	echo 'Golang installed'
 fi
 
 cd ${GOPATH}/src/github.com/hyperledger/fabric/
@@ -31,5 +33,5 @@ echo "===================== Crypto tools built successfully ====================
 echo 
 echo "Copying to bin folder of network..."
 echo
-cp ./build/bin/configtxgen $BASE_DIR
-cp ./build/bin/cryptogen $BASE_DIR
+cp -v ./build/bin/configtxgen /home/$USER/hyperledger-swarm/bin
+cp -v ./build/bin/cryptogen /home/$USER/hyperledger-swarm/bin
